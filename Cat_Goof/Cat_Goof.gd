c5a12bdaa8aa202a079cb61fe034ab079f15ad92
David <41875791+Astrol99@users.noreply.github.com>
2023-01-23 18:35:43 -0600

    Added basic movement physics

--- Cat_Goof/Cat_Goof.gd
+++ Cat_Goof/Cat_Goof.gd
@@ -1,24 +1,29 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
-export (int) var gravity = 3000;
-var velocity = Vector2.ZERO;
+var velocity = Vector2.ZERO
+const FLOOR_NORMAL = Vector2.UP
 
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	
-	velocity = velocity.normalized() * speed
+export var speed = Vector2(400.0, 500.0)
+export var gravity = 3500.0
 
-func _physics_process(delta):
-	get_input()
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right1") - Input.get_action_strength("move_left1"),
+		-Input.get_action_strength("jump1") if is_on_floor() and Input.is_action_just_pressed("jump1") else 0.0)
+
+func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2, is_jump_interrupted: bool) -> Vector2:
+	var velocity = linear_velocity
+	velocity.x = speed.x * direction.x
+	if direction.y != 0.0:
+		velocity.y = speed.y * direction.y
+	if is_jump_interrupted:
+		velocity.y = 0.0
+	return velocity
+
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted = Input.is_action_just_released("jump1") and velocity.y < 0.0
+	var direction = get_direction()
+	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
+	var snap = Vector2.DOWN * 60.0 if direction.y == 0.0 else Vector2.ZERO
 	velocity.y += gravity * delta
-	velocity.y = max(velocity.y, speed.y)
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide_with_snap(velocity, snap, FLOOR_NORMAL, true)
