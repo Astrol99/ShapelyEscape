947701a5e742a8a19c3a11a87ed9f9b656507351
David <41875791+Astrol99@users.noreply.github.com>
2023-01-28 14:35:59 -0600

    Used animationTree blend player

--- Cat_Goof/Cat_Goof.gd
+++ Cat_Goof/Cat_Goof.gd
@@ -1,23 +1,22 @@
 extends KinematicBody2D
 
-<<<<<<< HEAD
 const HALT_SPEED = 0.1
-const MAX_SPEED = 400.0
-=======
-export (int) var speed = 200
+const MAX_SPEED = 200.0
+const GRAVITY = 2000.0
+const JUMP_FORCE = 500.0
+
+export (int) var speed = 100
 var velocity = Vector2.ZERO;
->>>>>>> cc9e5366ace9f05a828a033f5a38a5dfb2d4fa96
-
-const GRAVITY = 1500.0
-const JUMP_FORCE = 1000.0
-
 var direction = Vector2.ZERO
-var velocity = Vector2.ZERO
-var jump_force = Vector2.ZERO
 
-<<<<<<< HEAD
+func _ready():
+	$AnimationTree.active = true
+
 func _process(delta):
 	direction = Vector2.ZERO
+	
+	if is_on_floor():
+		$AnimationTree.set("parameters/movement/current", int(direction.length() > 50))
 	
 	if Input.is_action_pressed("move_left1"):
 		direction.x = -1
@@ -27,19 +26,24 @@ func _process(delta):
 		direction.x = 1
 		$Sprite.flip_h = false
 	
+	if Input.is_action_pressed("jump1") and is_on_floor():
+		velocity.y = -JUMP_FORCE
+		$AnimationTree.set("parameters/in_air/current", 1)
+	
+	if Input.is_action_just_released("jump1"):
+		$AnimationTree.set("parameters/in_air/current", 0)
+	
 	if direction == Vector2.ZERO:
 		velocity = velocity.linear_interpolate(Vector2.ZERO, delta/HALT_SPEED)
 	else:
 		velocity = velocity.linear_interpolate(direction * MAX_SPEED, delta/HALT_SPEED)
 	
-	if Input.is_action_just_pressed("jump1") and is_on_floor():
-		velocity.y = -JUMP_FORCE
+	$AnimationTree.set("parameters/movement_time/scale", 1 + abs(direction.x)/100)
+	$AnimationTree.set("parameters/in_air_state/current", int(!$RayCast2D.is_colliding()))
 	
 	velocity.y += GRAVITY * delta
 	move_and_slide(velocity, Vector2.UP)
-=======
+
 func _physics_process(delta):
-	get_input()
-	velocity.y = max(velocity.y, speed.y)
 	velocity = move_and_slide(velocity)
->>>>>>> cc9e5366ace9f05a828a033f5a38a5dfb2d4fa96
+
