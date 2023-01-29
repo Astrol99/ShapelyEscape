27c0e27e4ed25accaa022ada9293771272591740
David <41875791+Astrol99@users.noreply.github.com>
2023-01-29 17:44:19 -0600

    Cleanup filestructure and imported tilemapping

--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -7,7 +7,7 @@ func _ready():
 	$VBoxContainer/Start.grab_focus()
 
 func _on_Enter_pressed():
-	if get_tree().change_scene("res://Level1.tscn") != OK:
+	if get_tree().change_scene("res://Level1/Level1.tscn") != OK:
 		print("An unexpected error occured when trying to switch to the World scene")
 
 func _on_Quit_pressed():
