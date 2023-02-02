a70399c8374f8519f299cbb402ccffa6288ad1b9
David <41875791+Astrol99@users.noreply.github.com>
2023-02-01 20:08:29 -0600

    blah

--- Actor.gd
+++ Actor.gd
@@ -1,7 +1,7 @@
 class_name Actor
 extends KinematicBody2D
 
-export var speed = Vector2(300.0, 600.0)
+export var speed = Vector2(300.0, 700.0)
 onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
 
 const FLOOR_NORMAL = Vector2.UP
