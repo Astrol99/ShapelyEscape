1e2b91f88d8ac81eb48341cb88ce5ed1018b31f2
David <41875791+Astrol99@users.noreply.github.com>
2023-02-06 17:57:36 -0600

    Updated screen to have messages and control tutorial sprites on first level, moved all central game record logic to screen

--- World_Objects/Screen/Screen.gd
+++ World_Objects/Screen/Screen.gd
@@ -1,16 +1,39 @@
 extends Node2D
 
-export (NodePath) onready var level = get_node(level)
 export (NodePath) onready var timer = get_node(timer)
+
+export (int) var max_score = 10
+export (int) var score = 0
+export (float) var time = 60
+
 onready var time_label = $Time
 onready var score_label = $Score
+onready var msg_label = $Message
+onready var shape_sprite = $Shape
+onready var shapes = [
+	preload("res://assets/shapes/circle.png"),
+	preload("res://assets/shapes/oval.png"),
+	preload("res://assets/shapes/rectangle.png"),
+	preload("res://assets/shapes/righttriangle.png"),
+	preload("res://assets/shapes/semicircle.png")
+]
+
+func _on_Timer_timeout():
+	if score < max_score:
+		msg_label.set_text("GAME OVER")
+
+func _on_shape_incinerated(shape):
+	if (shape in shape_sprite.texture.resource_path):
+		score += 1
+		shape_sprite.texture = shapes[randi() % shapes.size()]
 
 func _ready():
-	timer.start(level.time)
+	timer.start(time)
 
 func _physics_process(_delta):
-	time_label.set_text(format_second(timer.get_time_left()))
-	score_label.set_text("%d/%d" % [level.score, level.max_score])
+	if score < max_score:
+		time_label.set_text(format_second(timer.get_time_left()))
+		score_label.set_text("%d/%d" % [score, max_score])
 
 func format_second(total_seconds: float) -> String:
 	var seconds:float = fmod(total_seconds , 60.0)
