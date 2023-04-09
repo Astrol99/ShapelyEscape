cc4598bc870d039c722b766f643f1e3ce9d3c5e1
David <41875791+Astrol99@users.noreply.github.com>
2023-04-08 22:15:37 -0500

    Updated mainmenu to have parallaxbackground and anmimatedsprites

--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -4,6 +4,9 @@ func _ready():
 	randomize()
 	$VBoxContainer/Start.grab_focus()
 
+func _process(delta):
+	$ParallaxBackground/ParallaxLayer.motion_offset.x += -50 * delta
+
 func _on_Enter_pressed():
 	if get_tree().change_scene("res://Level1/Level1.tscn") != OK:
 		print("An unexpected error occured when trying to switch to the World scene")
